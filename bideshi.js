ed1448e4108fd6ac365cc130731780051cb9c496
Jamiul <jamiulhassnat@gmail.com>
2022-08-31 12:40:40 +0600

    Data successfully updated

--- bideshi.js
+++ bideshi.js
@@ -1,1 +1,21 @@
-console.log('Bideshi Data');
+const personFetchPromise =() =>{
+    fetch('https://randomuser.me/api/?results=10')
+    .then(person=>person.json())
+    .then(data=>myBideshini(data.results))
+}
+
+const myBideshini=(personDatas) =>{
+    for (const personData of personDatas){
+        const divId = document.getElementById('bideshi-data')
+        const newElement = document.createElement('div')
+        newElement.innerHTML=`
+        <h3>Gender: ${personData.gender} Location: ${personData.location.country}  </h3>
+        <h4>Gmail: ${personData.email}</h4>
+        `       
+        divId.appendChild(newElement);
+        divId.classList.add('add-some-style')
+    }
+   
+}
+
+personFetchPromise();
