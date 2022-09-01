2c1e27e9fbc5688008a2b823026d81dd2bd5943e
Jamiul <jamiulhassnat@gmail.com>
2022-09-01 14:57:09 +0600

    add css file And show country name with flag

--- countries.js
+++ countries.js
@@ -11,13 +11,28 @@ const displayCountries = countries =>{
        const countriesInfo = document.getElementById('countries-info');
        countries.forEach(country=>{
         const countryDiv = document.createElement('div');
-        countryDiv.classList.add('country');
+        countryDiv.classList.add('myCountry');
         countryDiv.innerHTML =`
-         <h3><span style="color:black">Name:<span> <span style="color:red"> ${country.name.common}</span> </h3>
-         <p>Capital: <span style="color:green"> ${country.capital[0]}</span> </p>
-        `
+         <h3><span style="color:black">Name:</span> <span style="color:red">${country.name.common}</span> </h3>
+         <p>Capital: <span style="color:green"> ${country.capital ?country.capital[0]:'No Capital'}</span> </p>
+           <button onclick="loadCountryDetail('${country.cca2}')"> Details </button>      
+         ` 
         countriesInfo.appendChild(countryDiv);
        })
 }
 
+const loadCountryDetail =(code) =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>countryDetails(data[0]))
+}
+
+const countryDetails=(countryData)=>{
+    const newDiv= document.getElementById('single-country-details');
+    newDiv.innerHTML=`
+    <h3>Country Name: ${countryData.name.common}</h3>
+    <img src="${countryData.flags.png}">
+    `
+}
 loadCountires();
