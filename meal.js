6d8a80b0e74935864e9335a4846b03bdab39c708
Jamiul <jamiulhassnat@gmail.com>
2022-09-01 17:47:26 +0600

    Dynamically added div

---
+++ meal.js
@@ -0,0 +1,29 @@
+const loadMeals = () =>{
+    const url ='https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>mealMenus(data.meals))
+}
+
+const mealMenus=(mealData)=>{
+    const divId= document.getElementById('meal-container');
+    mealData.forEach(meal =>{
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML= `
+        <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        divId.appendChild(mealDiv);
+
+    }
+
+    )
+
+}
+loadMeals();
